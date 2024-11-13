550e3e3d23787426b7f4c909d7dff34d95807f65
junsoo1003 <royalex@dgu.ac.kr>
2024-11-13 09:43:06 +0900

    feat: setting background-color

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/index.tsx";
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router} />
