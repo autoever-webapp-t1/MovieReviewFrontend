e780f90bc6290f10388a3c1f0142e176473b2c82
junsoo1003 <royalex@dgu.ac.kr>
2024-11-15 15:36:25 +0900

    [CHORE] MUI 의존성 추가 및 아이콘을 MUI 아이콘으로 변경 (#8)

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
-import Logo from "@assets/gasannes_logo_f3bb4b_notext.svg";
+import Logo from "@assets/logo.svg";
 import UserImage from "@assets/song.jpg";
 import ProfileImage from "@/widgets/profile-image";
-import SearchIcon from "@/widgets/search-icon";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function Header() {
   return (
@@ -32,7 +32,7 @@ export default function Header() {
           </nav>
           <div className={styles.headerUtilities}>
             <div className={styles.searchBar}>
-              <SearchIcon />
+              <SearchIcon sx={{ color: "var(--color-gray-500)" }} />
               <input className="" type="text" placeholder="영화 제목 검색" />
             </div>
             <div className={styles.profile}>
