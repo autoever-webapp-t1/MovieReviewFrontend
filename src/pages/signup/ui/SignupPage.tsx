f83e133c92bce92119fc83384d816923e5052f9b
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-11-15 10:55:23 +0900

    feat: add SignUpPage

--- src/pages/signup/ui/SignupPage.tsx
+++ src/pages/signup/ui/SignupPage.tsx
@@ -1,3 +1,24 @@
+import SplashBackground from "@/widgets/splash-background";
+import LogoImg from "@assets/logo.svg";
+import styles from "./SignupPage.module.css";
+import TextButton from "@/widgets/text-button/ui/TextButton";
+
 export default function SignupPage() {
-  return <div>signup</div>;
+  return (
+    <SplashBackground>
+      <div className={styles["signup-box"]}>
+        <div className={styles["logo-wrapper"]}>
+          <img src={LogoImg} alt="LogoImg" />
+        </div>
+        <div className={`${styles.title} header-h2`}>
+          좋아하는 영화를 골라주세요
+          <div className={styles["button-wrapper"]}>
+            <TextButton color="primary" onClick={() => {}}>
+              asdf
+            </TextButton>
+          </div>
+        </div>
+      </div>
+    </SplashBackground>
+  );
 }
