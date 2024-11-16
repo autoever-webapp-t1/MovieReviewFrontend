4f6977d9db17288e75eab398781daf1fa251a02c
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-11-16 16:15:01 +0900

    feat: add MemberDto

--- src/entities/user/model/types.ts
+++ src/entities/user/model/types.ts
@@ -2,3 +2,11 @@ export interface User {
   userId: number;
   email: string;
 }
+
+export interface MemberDto {
+  memberId: number; //카카오 고유 사용자 ID
+  nickname: string; //카카오 사용자 이름
+  email: string; //카카오 사용자 이메일
+  profile: string; //카카오 사용자 프로필사진
+  refreshToken: string; //카카오 사용자 refreshToke
+}
