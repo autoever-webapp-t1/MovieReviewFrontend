710f61d094495f10fa8f18f5ac08355e43cca7ce
junsoo1003 <royalex@dgu.ac.kr>
2024-11-17 03:35:48 +0900

    [fix] github 사용 중 발생한 에러 수정

--- src/shared/utils/dateUtils.ts
+++
@@ -1,8 +0,0 @@
-export function formatDate(dateString: string): string {
-  const [datePart, timePart] = dateString.split(" ");
-  const [year, month, day] = datePart.split("-");
-  return `${year}. ${parseInt(month, 10)}. ${parseInt(
-    day,
-    10
-  )}. ${timePart.slice(0, 5)}`;
-}
