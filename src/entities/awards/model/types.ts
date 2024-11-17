9ee904528aff3b107c25ec30978350398a1ea890
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-11-17 21:49:30 +0900

    feat: Awards Entities 추가

---
+++ src/entities/awards/model/types.ts
@@ -0,0 +1,11 @@
+export interface AwardsDto {
+  awardsId: number;
+  awardName: string;
+  nominated1: number;
+  nominated2: number;
+  nominated3: number;
+  nominated4: number;
+  startDateTime: string;
+  endDateTime: string;
+  topMovieId: number;
+}
