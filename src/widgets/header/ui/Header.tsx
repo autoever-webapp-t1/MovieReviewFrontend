4dfa6c28873e01b8ad4c2844ed383465286353c7
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-11-17 10:15:08 +0900

    fix: Logo 클릭 시 main으로 이동

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     <>
       <header className={styles.header}>
         <div className={styles.logo}>
-          <Link to="/">
+          <Link to="/main">
             <div className={styles.logoContainer}>
               <img src={Logo} alt="로고" width="90" />
             </div>
