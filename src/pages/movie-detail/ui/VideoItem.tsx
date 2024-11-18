646bace5e4aba11fea7649358ee72b20b4020c91
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-11-18 15:26:46 +0900

    feat: Video 보여줌

--- src/pages/movie-detail/ui/VideoItem.tsx
+++ src/pages/movie-detail/ui/VideoItem.tsx
@@ -7,17 +7,9 @@ interface VideoItemProps {
 export default function VideoItem({ src }: VideoItemProps) {
   console.log(src);
   return (
-    // <iframe
-    //   className={styles.container}
-    //   src={`https://www.youtube.com/watch?v=${src}`}
-    // />
-
     <iframe
-      width="560"
-      height="315"
-      // src={`https://www.youtube.com/embed/${src}`}
-      src={`https://www.youtube.com/embed/FuJ1RiLoq-M`}
-      title="YouTube video player"
+      className={styles.container}
+      src={`https://www.youtube.com/embed/${src}`}
     ></iframe>
   );
 }
