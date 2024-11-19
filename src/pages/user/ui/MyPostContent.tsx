6acfe9a4aae4847ca16a663c15deb22d1a9eb40c
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-11-20 08:01:25 +0900

    feat: MyPost

--- src/pages/user/ui/MyPostContent.tsx
+++ src/pages/user/ui/MyPostContent.tsx
@@ -1,13 +1,24 @@
+import { useMemo } from "react";
 import styles from "./MyPostContent.module.css";
+import { useMyPost } from "@/entities/user/api/hooks";
+import MyPostItem from "./MyPostItem";
 
 export default function MyPostContent() {
-  // const { data } = useMyPost();
+  const { data, hasNextPage, fetchNextPage } = useMyPost();
 
-  // const posts = useMemo(() => {
-  //   if (data) {
-  //     return data.pages.flatMap((page) => page.dtoList);
-  //   } else return [];
-  // }, [data]);
+  const posts = useMemo(() => {
+    if (data) {
+      return data.pages.flatMap((page) => page.dtoList);
+    } else return [];
+  }, [data]);
 
-  return <div className={styles.container}></div>;
+  console.log(posts);
+
+  return (
+    <div className={styles.container}>
+      {posts.map((post, i) => (
+        <MyPostItem key={i} post={post} />
+      ))}
+    </div>
+  );
 }
